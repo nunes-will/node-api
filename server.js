b91a55a51ca9637b5fc99b4b7dd36132819194b8
nunes-will <william-x7@hotmail.com>
2020-03-21 11:24:08 -0300

    Added connection to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+
+// Starting the app
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello William');
-})
 
-app.listen(8000);
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+  
+
+
+// Connecting to the database
+mongoose
+  .connect('mongodb://127.0.0.1:27017/node-api', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+   .then(() => console.log('DB connected!'))
+   .catch(err => {
+     console.log(`DB Connection Error: ${err.message}`);
+   });
+
+
+// Adding our model
+require('./src/models/Product');
+
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
---
+++ src/models/Product.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+
+// Creates a model to our database
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+
+// Register a model to our app
+mongoose.model('Product', ProductSchema);
